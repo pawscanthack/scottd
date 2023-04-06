3f31c4b7181a22c9d6b939d2e477b1c6b797d796
pawscanthack <scottthomas.davis@bellevuecollege.edu>
2023-04-06 15:18:33 -0400

    Week 2 color.py complete

---
+++ Week2/color.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# A script that will prompt for a user name and then for their favorite color then print out
+
+# Versioning
+# SDavis-20230406: initial version
+
+# Set up initial variables and imports
+
+
+# Main routine that is called when script is run
+def main():
+    username = get_username()
+    favcolor = get_favcolor()
+    display_userinfo(username, favcolor)
+
+# Subroutines
+
+def get_username():
+      return input("\nWhat is your name?\n")
+
+def get_favcolor():
+      return input("\nWhat is your favorite color?\n")
+
+def display_userinfo(name, color):
+      print(f'\nThe favorite color for {name} is {color}.')
+
+# Run main() if script called directly, else use as a library to be imported
+if __name__ == "__main__":
+        main()
