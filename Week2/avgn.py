fa6504beb9d67ad7efb98f6ac016262c2aa37b12
pawscanthack <scottthomas.davis@bellevuecollege.edu>
2023-04-06 17:54:53 -0400

    Added comments to avgn.py

--- Week2/avgn.py
+++ Week2/avgn.py
@@ -5,6 +5,7 @@ SEC444
 Bellevue College
 Spring 2023
 """
+
 # Script that reads arguments, all positive numbers, on the command line and finds their average to two decimal places
 
 # Versioning
@@ -25,23 +26,23 @@ def main():
         numbers = []
         for arg in sys.argv[1:]:
             try:
-                num = float(arg)
-                if num < 0:
+                num = float(arg)  # Convert argument to float
+                if num < 0:  # Check if argument is negative
                     raise ValueError("All arguments must be positive numbers")
-                numbers.append(num)
+                numbers.append(num)  # Add argument to list of valid numbers
             except ValueError:
-                print(f"Error: {arg} is not a valid number")
-                sys.exit(1)
+                print(f"Error: {arg} is not a valid number")  # Print error message for invalid argument
+                sys.exit(1)  # Exit program with error code
 
-        # Call avg function
+        # Call avg function with valid numbers and print result
         average = avg(*numbers)
-        numbers_str = ", ".join(str(num) for num in numbers)
-        print(f"Average of {numbers_str} is {average:.2f}")
+        numbers_str = ", ".join(str(num) for num in numbers)  # Convert list of numbers to comma-separated string
+        print(f"Average of {numbers_str} is {average:.2f}")  # Print formatted average
 
     except ValueError as ve:
-        print("Error:", ve)
+        print("Error:", ve)  # Print error message for invalid argument or negative argument
         sys.tracebacklimit = 0 # Suppress traceback
-        sys.exit(1)
+        sys.exit(1)  # Exit program with error code
 
 
 
@@ -52,15 +53,13 @@ def avg(*args):
         # Check for arguments
         if len(args) < 1:
             raise ValueError("At least one number must be provided")
-            
+
         # Validate argument data type and sign
         for num in args:
             if not isinstance(num, (int, float)):
                 raise TypeError("All arguments must be numbers")
             if num < 0:
                 raise ValueError("All arguments must be positive numbers")
-
-        
 
         # Calculate average
         total = sum(args)
@@ -69,7 +68,8 @@ def avg(*args):
         return rounded_average
 
     except (TypeError, ValueError) as e:
-        print(f"Error: {e}")
+        print(f"Error: {e}")  # Print error message for invalid or negative argument
+
 
 
 # Run main() if script called directly, else use as a library to be imported
