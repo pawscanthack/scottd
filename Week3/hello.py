5d074d7fdb60d3075aac64d876be2b95c59edfb1
pawscanthack <scottthomas.davis@bellevuecollege.edu>
2023-04-12 19:12:25 -0700

    debugging for hello.py complete

--- Week3/hello.py
+++ Week3/hello.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # Script to print out Hello World.
 a = 11
-msg = "Hello Wrld"
-if a < 10:
-print msg
+msg = "Hello World"
+if a > 10:
+    print (msg)
 else:
-    print error
+    print("error")
