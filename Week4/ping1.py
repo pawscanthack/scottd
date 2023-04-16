20fff26c853f96f6d7526a6bc558267d011b63b5
pawscanthack <scottthomas.davis@bellevuecollege.edu>
2023-04-16 19:48:56 -0400

    Created ping1.py template, added pingfile.txt

---
+++ Week4/ping1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Scott Davis
+SEC444
+Bellevue College
+Spring 2023
+"""
+# a script that will read from a file of computer IPs and names.  It then will ping each one and print the IP or DNS name and the time to ping to the screen
+
+# Versioning
+# Scott-20230416: initial version
+
+# Set up initial variables and imports
+import sys
+SITES = ['bhs','ah','lms']
+MAIL_SERVER = 'smtp.google.com'
+
+# Main routine that is called when script is run
+def main():
+ 
+# Get the messages and process them
+
+
+# Subroutines
+
+
+# Run main() if script called directly, else use as a library to be imported
+if __name__ == "__main__":
+        main()
