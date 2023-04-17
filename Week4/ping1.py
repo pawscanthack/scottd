dc9643b31d16568650a88cd8198d16b8499508f4
pawscanthack <scottthomas.davis@bellevuecollege.edu>
2023-04-16 17:55:29 -0700

    ping1.py working with formatting (all done in nano/AWS)

--- Week4/ping1.py
+++ Week4/ping1.py
@@ -7,7 +7,6 @@ Spring 2023
 """
 # a script that will read from a file of computer IPs and names.  It then will ping each one and print the IP or DNS name and the time to ping to the screen
 
-# Versioning
 # Scott-20230416: initial version
 
 # Set up initial variables and imports
@@ -17,16 +16,17 @@ from pinglib import pingthis
 # Main routine that is called when script is run
 def main():
     target_file = argument_check()
+    print("IP, TimeToPing (ms)")
+    # Open file and do stuff
     with open(target_file, 'r') as file:
         for line in file:
             target = line.strip()
             result = pingthis(target)
-            print(result)
-# Get the messages and process them
-
+            print(f"{result[0]}, {result[1]}")
 
 # Subroutines
 def argument_check():
+    """Function checks for presence of argument and gives usage if argument is missing"""
     if  not len(sys.argv) == 2:
         print("Usage: ping1.py [file.txt]")
         sys.exit(1)
