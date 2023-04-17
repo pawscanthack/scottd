1f6c3be2205319ffb05e3395ad32ea517c9b3d70
pawscanthack <scottthomas.davis@bellevuecollege.edu>
2023-04-17 01:44:19 -0400

    ping4_v2.py working (imports from pinglib)

--- Week4/ping4_v2.py
+++ Week4/ping4_v2.py
@@ -6,12 +6,11 @@ Bellevue College
 Spring 2023
 """
 # Script accepts a filename, an IP address, or a server name as input, and tests the connectivity to the specified target plus takes an optional second command line argument that is a csv output file
-# Displays IP, Domain Name, and Time
 # Scott-20230416: initial version 
 
 # Set up initial variables and imports
 import sys
-import subprocess
+from pinglib import pingthis
 import csv
 import socket
 
@@ -27,8 +26,10 @@ def main():
         sys.exit(0)
     elif file_check(user_input) == 0:
         result = pingthis(user_input)
-        csv_output.append(result)
-        print(f"{result[0]}, {result[1]}, {result[2]}")
+        target_dns = get_domain(result[0])
+        ping_result = [result, target_dns]
+        csv_output.append(ping_result)
+        print(f"{result[0]}, {target_dns}, {result[1]}")
         if filename != 0:
             write_to_csv(filename)
 
@@ -55,8 +56,10 @@ def ping_from_file(target_file, filename):
         for line in file:
             target = line.strip()
             result = pingthis(target)
-            csv_output.append(result)
-            print(f"{result[0]}, {result[1]}, {result[2]}")
+            target_dns = get_domain(target)
+            ping_result = [result, target_dns]
+            csv_output.append(ping_result)
+            print(f"{result[0]}, {target_dns}, {result[1]}")
             if filename != 0:
                 write_to_csv(filename)
 
@@ -75,41 +78,12 @@ def write_to_csv(filename):
             csv_writer.writerow([str(item) for item in row])
     return
 
-def pingthis(ip):
-    """Pings the IP or DNS name passed to it"""
-    # Call the ping command
-    ping = subprocess.run(["ping", "-c", "1", ip], capture_output=True)
-    # Check the return code
-    if ping.returncode == 0:
-        ping_decoded = ping.stdout.decode()
-        return process_data(ping_decoded)
-    else:
-        return [ip, "Not Found", "Not Found"]  
-    
-def process_data(ping_data):
-    """Processes the output from the ping command"""
-    # Split the output into lines
-    lines = ping_data.splitlines()
-
-    # Find the line that contains the PING target (i.e., starts with "PING")
-    target_line = next(line for line in lines if line.startswith("PING"))
-
-    # Find the line that contains the ping response (i.e., starts with "64 bytes from")
-    time_line = next(line for line in lines if line.startswith("64 bytes from"))
-
-    # Extract the IP address and PING target from the ping response lines
-    ip_address = target_line.split('(')[1].split(')')[0]
+def get_domain(ip_address):
     try:
         domain_name = socket.gethostbyaddr(ip_address)[0]
     except socket.herror:
         domain_name = "Unknown"
-
-    # Extract the time value from the ping response lines
-    time_string = time_line.split()[-2]
-    time_value = time_string.split('=')[1]
-
-    # Return the time value and PING target as a list
-    return [ip_address, domain_name, float(time_value)]
+    return domain_name
 
 # Run main() if script called directly, else use as a library to be imported
 if __name__ == "__main__":
